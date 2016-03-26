8197401e00c9fe5b52f8c2c20b40b099e2930c0c
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2016-03-26 01:06:36 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # stair_traversal_tutorial
 Repo providing a ROS workspace setup that allows for the simulation of a tracked robot capable of autonomous stair climbing
+
+This repository provides a complete workspace setup for providing a simulated tracked robot that is capable of autonomous stair climbing in Gazebo simulation.
+
+
+# Installation
+Make sure you installed ROS Indigo as described in the [ROS Installation instructions](http://wiki.ros.org/indigo/Installation/Ubuntu).
+
+Add the OSRF repository to your system to allow the use of newer Gazebo versions. This is described [here](http://gazebosim.org/tutorials?tut=install_ubuntu&cat=install).
+
+Install a Gazebo version of your choice (The tutorial has been well tested with gazebo5):
+```
+sudo apt-get install ros-$ROS_DISTRO-gazebo5-ros ros-$ROS_DISTRO-gazebo5-plugins ros-$ROS_DISTRO-gazebo5-ros-control
+```
+
+Enter a folder of your choice (a good choice is the home folder) and check out this repo:
+```
+git clone https://github.com/tu-darmstadt-ros-pkg/autonomous_stair_traversal_tutorial.git
+```
+Enter folder and run the update script:
+```
+cd autonomous_stair_traversal_tutorial
+./update.sh
+```
+The script creates a workspace, installs dependencies, clones multiple repositories and builds them using [catkin tools](https://catkin-tools.readthedocs.org/en/latest/quick_start.html)
+
+# Usage
+
+Further instructions on how to use the workspace will be added soon.
---
+++ update.sh
@@ -0,0 +1,91 @@
+source /opt/ros/indigo/setup.bash
+
+git pull
+
+# This should be extended to first check if everything is installed and only do the sudo requiring call when there's anything missing.
+echo "Installing needed packages (both ROS package and system dependency .deb packages) ..."
+
+PACKAGES_TO_INSTALL="\
+mercurial \
+git \
+python-rosdep \
+python-wstool \
+python-catkin-tools \
+libnlopt-dev \
+ros-$ROS_DISTRO-desktop \
+ros-$ROS_DISTRO-moveit-ros \
+ros-$ROS_DISTRO-moveit-simple-controller-manager \
+ros-$ROS_DISTRO-moveit-setup-assistant \
+ros-$ROS_DISTRO-moveit-commander \
+ros-$ROS_DISTRO-moveit-planners-ompl \
+ros-$ROS_DISTRO-nav-core \
+ros-$ROS_DISTRO-base-local-planner \
+ros-$ROS_DISTRO-hector-nav-msgs \
+ros-$ROS_DISTRO-hector-map-tools \
+ros-$ROS_DISTRO-message-to-tf \
+ros-$ROS_DISTRO-driver-base \
+ros-$ROS_DISTRO-laser-filters \
+ros-$ROS_DISTRO-image-proc \
+ros-$ROS_DISTRO-image-transport-plugins \
+ros-$ROS_DISTRO-depth-image-proc \
+ros-$ROS_DISTRO-ros-control \
+ros-$ROS_DISTRO-ros-controllers \
+ros-$ROS_DISTRO-effort-controllers \
+ros-$ROS_DISTRO-joint-trajectory-controller \
+ros-$ROS_DISTRO-joint-state-controller \
+ros-$ROS_DISTRO-perception-pcl
+ros-$ROS_DISTRO-map-server \
+ros-$ROS_DISTRO-amcl \
+ros-$ROS_DISTRO-sbpl \
+ros-$ROS_DISTRO-perception-pcl \
+ros-$ROS_DISTRO-pointcloud-to-laserscan \
+ros-$ROS_DISTRO-joy \
+ros-$ROS_DISTRO-teleop-twist-joy \
+ros-$ROS_DISTRO-costmap-2d \
+ros-$ROS_DISTRO-octomap \
+ros-$ROS_DISTRO-octomap-msgs \
+ros-$ROS_DISTRO-octomap-ros \
+ros-$ROS_DISTRO-move-base-msgs \
+ros-$ROS_DISTRO-hector-worldmodel-msgs \
+ros-$ROS_DISTRO-rqt-joint-trajectory-controller \
+ros-$ROS_DISTRO-rqt-robot-steering \
+ros-$ROS_DISTRO-robot-localization"
+
+dpkg -s $PACKAGES_TO_INSTALL 2>/dev/null >/dev/null || sudo apt-get -y install $PACKAGES_TO_INSTALL
+
+
+if ! [ -e "./.rosinstall" ]; then
+  wstool init
+fi
+
+wstool merge default_packages.rosinstall 
+
+wstool update
+
+# We build using catkin_tools per default
+#catkin_make_isolated -DCMAKE_BUILD_TYPE=Release
+catkin build -DCMAKE_BUILD_TYPE=Release
+
+
+PWD=$(pwd)
+
+# Initialization done, print info
+cat <<EOF
+
+===================================================================
+Workspace initialization completed.
+You can setup your current shell's environment to use the workpace
+by entering
+
+    source ${PWD}/devel/setup.bash
+
+or by adding this command to your .bashrc file for automatic setup on
+each invocation of an interactive shell:
+
+    echo "source ${PWD}/devel/setup.bash" >> ~/.bashrc
+
+You can also modify your workspace config (e.g. for adding additional
+repositories or packages) using the wstool command.
+===================================================================
+
+EOF
